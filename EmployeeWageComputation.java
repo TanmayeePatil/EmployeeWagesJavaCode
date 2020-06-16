7354f0ec791f44dbf3ab7ecf2e927d7c91dafda4
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-16 07:20:10 +0530

    Added: Random function for employee attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,32 @@
+import java.util.Random;
+
 public class EmployeeWageComputation{
+
+ EmployeeWageComputation()
+ {
+   System.out.println("Default Constructor called");
+ }
+
+ public void empAttendance()
+ {
+   Random myRan=new Random();
+   int myRanVar=myRan.nextInt(2);
+   System.out.println("Random Integers: "+myRanVar);
+
+      if(myRanVar==0)
+       {
+         System.out.println("Employee Absent");
+       }
+      else
+       {
+         System.out.println("Employee Present");
+       }
+ }
 
  public static void main (String args[]){
      System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
+     EmployeeWageComputation ewc=new EmployeeWageComputation();
+     ewc.empAttendance();
  }
 
 }
