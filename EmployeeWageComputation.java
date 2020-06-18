cc6cff4682254a66c93b1266d21560e56b488ec0
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-18 20:25:52 +0530

    Added: ArrayList

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,24 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 interface EmpWageBuilder{
-  public void calSalary(EmployeeBuilder[] empBuild); //interface method
+  public void calSalary(List<EmployeeBuilder> empBuild); //interface method
 }
 
 class ComanyWiseWage implements EmpWageBuilder{
 
     @Override
-    public void calSalary(EmployeeBuilder[] empBuild) {
-    int i,empSalary,totalHr,numDays;
-    int absentCount=0,partPresentCount=0,fullPresentCount=0,empHrs=1;
+    public void calSalary(List<EmployeeBuilder> empBuild) {
+    int i,empSalary;
 
-       for(i=0;i<empBuild.length;i++)
+       for(i=0;i<empBuild.size();i++)
        {
-        totalHr=0;
-        numDays=0;
-        empSalary=0;
+        int totalHr=0,numDays=0;
+        int absentCount=0,partPresentCount=0,fullPresentCount=0,empHrs=1;
 
-        while(totalHr<=empBuild[i].totalMaxHr && numDays<empBuild[i].daysPerMonth)
+        while(totalHr<=empBuild.get(i).totalMaxHr && numDays<empBuild.get(i).daysPerMonth)
         {
           Random myRan=new Random();
           int resultAttendance=myRan.nextInt(3);
@@ -40,14 +40,13 @@ class ComanyWiseWage implements EmpWageBuilder{
           }
          totalHr=totalHr+empHrs;
         }
-        empSalary=empBuild[i].empWagePerHrs*totalHr;
-        System.out.println("Company Name: "+ empBuild[i].companyName);
+        empSalary=empBuild.get(i).empWagePerHrs*totalHr;
+        System.out.println("Company Name: "+ empBuild.get(i).companyName);
         System.out.printf("Absent Days:%-5d PartTime Days:%-5d FullTimeDays:%d",absentCount,partPresentCount,fullPresentCount);
         System.out.println("\nTotal salary for "+numDays+" days or "+totalHr+" working hours is:"+ empSalary);
         System.out.println("------------------------------------------------------------------------------");
        }
     }
-
 }
 
 public class EmployeeWageComputation {
@@ -57,15 +56,13 @@ public class EmployeeWageComputation {
      System.out.println("                        WELCOME TO EMPLOYEE WAGE PROBLEM                      ");
      System.out.println("------------------------------------------------------------------------------");
 
-     Scanner sc = new Scanner(System.in);
-     EmployeeBuilder empBuild[];
+     List<EmployeeBuilder> empBuild = new ArrayList<>();
 
+     Scanner sc = new Scanner(System.in);
      System.out.println("Number of companies:");
      int num = sc.nextInt();
-     empBuild=new EmployeeBuilder[num];
 
      for (int i=0; i<num; i++) {
-            empBuild[i] = new EmployeeBuilder();
             System.out.println("\nCOMPANY RECORD: "+(i+1));
             System.out.println("Enter company name:");
             String comapanyName=sc.next();
@@ -76,7 +73,7 @@ public class EmployeeWageComputation {
             System.out.println("Enter maximum hours:");
             int totalMaxHr=sc.nextInt();
             EmployeeBuilder ewcArr=new EmployeeBuilder(comapanyName,empWagePerHrs,daysPerMonth,totalMaxHr);
-            empBuild[i]=ewcArr;
+            empBuild.add(ewcArr);
         }
       System.out.println("---------------------------- WAGE CALCULATION --------------------------------");
       ComanyWiseWage cw=new ComanyWiseWage();
